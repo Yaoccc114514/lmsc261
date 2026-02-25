67b72add03ea4eadabf05ae2c1314acd90573321
Yaoccc114514 <2923326938@qq.com>
2026-02-25 12:20:28 -0800

    Add files via upload

---
+++ Week-5/name_length.py
@@ -0,0 +1,9 @@
+# Ask the user for their full name
+full_name = input("Enter your full name: ")
+
+# Calculate the length of the name
+name_length = len(full_name)
+
+# Print the length and its type
+print("Length of your name:", name_length)
+print("Type of the length:", type(name_length))
